a682c807ea5efd0955bafd89b31f020f736bfb1e
fangran <fangran0813@foxmail.com>
2018-12-07 10:39:29 +0800

    update

--- src/main/java/com/cnuip/gaea/service/swagger/SwaggerConfig.java
+++ src/main/java/com/cnuip/gaea/service/swagger/SwaggerConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,7 +20,6 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo(properties))
                 .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.cnuip"))
                 .paths(Predicates.and(Predicates.not(PathSelectors.regex("/log")),
                         Predicates.not(PathSelectors.regex("/error"))))
                 .build();
