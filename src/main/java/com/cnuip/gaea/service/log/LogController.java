61285a382a0bf9c9103a6c4dd904ba95a5b2221e
fangran <fangran0813@foxmail.com>
2018-12-07 09:43:38 +0800

    update

--- src/main/java/com/cnuip/gaea/service/log/LogController.java
+++ src/main/java/com/cnuip/gaea/service/log/LogController.java
@@ -1,6 +1,5 @@
 package com.cnuip.gaea.service.log;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LogController {
 
-    @Autowired
-    BuildProperties buildProperties;
+    private BuildProperties buildProperties;
+
+    public LogController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
 
     @GetMapping("/log")
     public String log(Model model) {
         model.addAttribute("plugin", buildProperties.getArtifact());
         model.addAttribute("version", buildProperties.getVersion());
-        return "/ws2.jsp";
+        return "ws2";
     }
 }
